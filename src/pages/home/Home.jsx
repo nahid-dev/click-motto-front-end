d0fba133cf8c3be1b601b880760975cdd3523ba6
nahid-dev <nahidv232@gmail.com>
2023-08-25 20:30:45 +0600

    update two

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
 import Header from "../../components/header/Header";
 import CategorySlider from "../../components/categories/CategorySlider";
 import AllPicture from "../../components/allPicture/AllPicture";
 
 const Home = () => {
-  const [query, setQuery] = useState("people");
   return (
     <>
       {/* Header area */}
-      <Header setQuery={setQuery}></Header>
+      <Header></Header>
       {/* Category Area */}
       <CategorySlider></CategorySlider>
       {/* All picture area */}
